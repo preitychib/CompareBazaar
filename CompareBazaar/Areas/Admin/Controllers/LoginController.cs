04c84ae680e70e7e24eb309759903f58330dce23
preitychib <preeti.chib2015@gmail.com>
2022-01-24 19:19:39 +0530

    Add project files.

---
+++ CompareBazaar/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompareBaazar.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/login")]
+    public class LoginController : Controller
+    {
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CompareBazaar/Entities/Products.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompareBazaar.Entities
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(300,MinimumLength =4)]
+        public string Name { get; set; }
+        [Required]
+        public string ImagePath { get; set; }
+       
+        public int Discount { get; set; }
+        [Column(TypeName = "money")]
+        [Required]
+        public decimal Price { get; set; }
+       
+        public string details { get; set; }
+        public bool Availabitlty { get; set; }
+
+        
+    }
+}
