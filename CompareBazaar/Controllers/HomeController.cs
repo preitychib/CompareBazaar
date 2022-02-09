839489077db169cf5fddae54d47c5952cb3cb70d
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-09 14:40:35 +0530

    example code for fetching api data

--- CompareBazaar/Controllers/HomeController.cs
+++ CompareBazaar/Controllers/HomeController.cs
@@ -1,15 +1,17 @@
 ﻿using CompareBazaar.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CompareBazaar.Controllers
 {
-   
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -19,9 +21,28 @@ namespace CompareBazaar.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // return View();
+            using (var client = new HttpClient())
+            {
+                var url = "https://comparebazaar-api.herokuapp.com/api/flipkart/mobile/";
+
+                // return await client.GetAsync(url);
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                var mobiles = JsonConvert.DeserializeObject<object>(content);
+
+                // ViewData["response"] = response;
+                Console.WriteLine(mobiles);
+
+                ViewBag.mobiles = mobiles;
+
+                return View();
+            }
         }
 
         public IActionResult About()
